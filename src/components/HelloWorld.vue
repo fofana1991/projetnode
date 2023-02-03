b03fcd9f3a09483617f7a135fb1f734d19273f89
Fofana <mamadou19fofana91@gmail.com>
2023-02-03 15:14:01 +0000

    projet sociaux crection

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,60 +2,62 @@
 
 <template class="bg-secondary">
 
-<div class="container bg-primary">
+<div class="container bg-primary ">
   <div class="row">
     <div class=" col-md-6 col-lg-12">
       
-<form id='form' @submit.prevent='postData'>
+<form id='form' @submit.prevent='postData' class="p-3 m-3">
 
-  <div class='form-group'>
-<input class="form-control form-control-lg" type='hidden' v-model='thing.id'>
-</div>
-<div class='form-group'>
- 
+        <div class='form-group p-3 m-3 '>
+           <input class="form-control form-control-lg" type='hidden' v-model='thing.id'>
+       </div>
+       <div class='form-group p-3 m-3'>
   
- <div> 
+           <div> 
   
- <input id="wc-input"   type="color" v-model='thing.color' /><br />
+          <input id="wc-input"   type="color" v-model='thing.color' /><br />
   
-   </div> 
-  </div>
+           </div> 
+       </div>
   
 
-<div class='form-group'>
-<label for='title'>title:</label><input class="form-control form-control-lg"  type='texte'  id='title' v-model='thing.title'>
-</div>
+       <div class='form-group p-3 m-3'>
+           <input class="form-control form-control-lg"  type='texte'  id='title' v-model='thing.title' placeholder="title">
+      </div>
 
-<div class='form-roup'>
-<label for='description'>image</label><img width="100" :src='FILE' height="100" id='blash'/>
-</div>
-
-
-<div class='form-roup'>
-<label for='description'>description:</label><input class="form-control form-control-lg"  type='texte'  v-model='thing.description'>
-</div>
-
-<div class='form-group'>
-<label for='filename'>filename</label><input class="form-control form-control-lg"  type='texte' id='filename' name='filename' v-model='thing.description'>
-</div>
+       <div class='form-group p-3 m-3'>
+          <img width="100" :src='FILE' height="100" id='blash'/>
+       </div>
 
 
-<div class='form-group'>
+      <div class='form-group p-3 m-3'>
+          <input class="form-control form-control-lg"  type='texte'  v-model='thing.description' placeholder="description">
+      </div>
 
-<label for='image'>file</label><input class="form-control form-control-lg" type='file' @change='onFileUpload' name='file' id="file">
-</div>
-<div>
-<input class="form-control form-control-lg"  type='hidden'  v-model='thing.imageUrl'>
-</div>
+      <div class='form-group p-3 m-3'>
+          <input class="form-control form-control-lg"  type='texte' id='filename' name='filename' v-model='thing.description' placeholder="filname" >
+      </div>
 
-<div class='form-group'>
-<input type='hidden'  v-model='thing.userId' >
-</div>
 
-<div class='form-group'>
-<label for='price'>price:</label><input class="form-control form-control-lg"  type='number' name='price' v-model='thing.price'>
-</div>
-<button  @click='(e)'> Ajouter</button>
+       <div class='form-group p-3 m-3'>
+
+          <input class="form-control form-control-lg" type='file' @change='onFileUpload' name='file' id="file">
+       </div>
+       <div>
+           <input class="form-control form-control-lg"  type='hidden'  v-model='thing.imageUrl'>
+       </div>
+
+       <div class='form-group p-3 m-3'>
+           <input type='hidden'  v-model='thing.userId' >
+       </div>
+
+        <div class='form-group p-3 m-3'>
+            <input class="form-control form-control-lg"  type='number' name='price' v-model='thing.price' placeholder="price" >
+       </div>
+
+       <div class='form-group p-3 m-3'>
+             <button  @click='(e)'> Ajouter</button>
+       </div>     
 
 </form>
 
